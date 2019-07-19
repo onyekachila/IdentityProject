a678720c7f11a085d2be2893dde8c75bdc3c330b
onyekachila <onyekachila@gmail.com>
2019-07-19 14:40:18 +0100

    Added create user as well as get users

---
+++ IdentityProject.API/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace IdentityProject.API.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+           : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- IdentityProject.API/Infrastructure/ApplicationUser.cs
+++ IdentityProject.API/Infrastructure/ApplicationUser.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace IdentityProject.API.Infrastructure
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+
     }
 }
